ca012d36c963caadeac7eb970ee71682f9371b5c
Dzyza Yaroslav <drom.yasa@gmail.com>
2021-04-15 16:55:07 +0300

    feat: add menu-item gereration

--- app/components/menu-inner/menu.js
+++ app/components/menu-inner/menu.js
@@ -4,16 +4,33 @@ class MenuItem {
     this.$element = $element;
 
     //this.onLoad = this.onLoad.bind(this)
-    $(window).on('main:ready', this.onLoad );
+    $(window).on('main:ready', this.onLoad);
+
 
   }
-  //$('ul.menu-inner__list').append($('<li>')).attr('class', 'menu-inner__item');
+
+  // $('ul.menu-inner__list').append($('<li>')).attr('class', 'menu-inner__item');
+
 
   onLoad = (e, data) => {
-    data.forEach(element => {
-      $('ul.menu-inner__list').html('<li class="menu-inner__item"' + data.items[index] + '</li>');
+    const {$element} = this;
+    const {items, texts, links} = data.menu;
+
+    //const $container = $element.find('ul.menu-inner__list');
+    const $container = $element.find('div.links-button');
+    items.forEach(({text:textID, isLine, value}, index) => {
+      const text = texts[textID]?.ru;
+      const link = links[index]?.ru;
+      const str = `<li class="menu-inner__item${isLine ? " menu-inner__item-line" : ""}"> <a href="${isLine ? "" : link}">${text || ""}</a></li>`
+      $container.before(str);
     })
-    console.log(this);
+    const $link = $element.find('li.menu-inner__item a');
+    $link.click((e) => {
+      const $target = $(e.currentTarget);
+      const link = $target.attr("href");
+      $target.trigger('change-page', [link]);
+    })
+    $link.css({pointerEvents: 'none'});
   }
 
 }
@@ -23,3 +40,12 @@ $('.menu-inner').each((index, element) => {
   console.log($(element));
 })
 
+/*
+$('li.menu-inner__item').on('change-page', (event, link) => {
+  console.log(link);
+})
+
+$('li.menu-inner__item').click((event) => {
+  $(event.currentTarget).trigger('change-page', [$(event.currentTarget).attr("href")]);
+})
+*/
