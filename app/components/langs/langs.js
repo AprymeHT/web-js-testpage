e582d7588ed876b9c25a0cd9065d97ce18ce5faa
Dzyza Yaroslav <drom.yasa@gmail.com>
2021-04-16 15:08:30 +0300

    feat: add lang changing via menu

--- app/components/langs/langs.js
+++ app/components/langs/langs.js
@@ -8,35 +8,34 @@ class Language {
 
   onload = (e, data) => {
     const {$element} = this;
-    const {items, texts, lang, langItem} = data.menu;
+    const {langItem} = data.menu;
 
     const $container = $element.find('ul');
-    langItem.forEach(({change}, index) => {
-      console.log(change);
+    langItem.forEach(({change}) => {
       const str = `<li class="langs__item">${change}</li>`;
       $container.append(str);
     })
 
-    console.log($container);
+    $element.on('click', e => {
+      const $target = $(e.currentTarget);
+      $target.addClass('langs_active');
+    })
 
+    const $paragraph = $element.find('p');
+    const $langItem = $element.find('li.langs__item');
 
+    $langItem.on('click', e => {
+      const $target = $(e.currentTarget);
+      const pText = $paragraph.text();
+      $paragraph.text($target.text());
+      $element.removeClass('langs_active');
+      e.stopPropagation();
+      $target.trigger('change-language', [pText]);
 
-   /* console.log(texts);
-
-    const $lang = $('.button_small');
-
-    lang.forEach(({text, value, tag}, index) => {
-      const text1 = texts[index]?.en;
-      console.log(text1);
-      const tagName = tag;
-      console.log(tagName);
-      $(tagName).text(text1);
-
-    })*/
-
-
+    })
   }
 }
+
 
 $('div.langs').each((index, element) =>{
   new Language($(element));
