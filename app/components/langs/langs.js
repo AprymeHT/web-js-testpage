74d4c4f22d0d3c12dcfa073cfb2f934ada542726
Yaroslav Dzyza <drom.yasa@gmail.com>
2021-04-19 15:06:50 +0300

    fix: some minor fixes

--- app/components/langs/langs.js
+++ app/components/langs/langs.js
@@ -1,20 +1,28 @@
+import '../index-part/index.js'
+
 class Language {
   constructor($element) {
     console.log($element);
     this.$element = $element;
 
     $(window).on('main:ready', this.onload);
+    
+    $(window).on('change-language', this.changeLang);
   }
 
   onload = (e, data) => {
     const {$element} = this;
     const {langItem} = data.menu;
+    const pText = "ru";
 
     const $container = $element.find('ul');
     langItem.forEach(({change}) => {
       const str = `<li class="langs__item">${change}</li>`;
       $container.append(str);
     })
+
+    $(window).trigger('change-language', [data, pText]);
+
 
     $element.on('click', e => {
       const $target = $(e.currentTarget);
@@ -30,8 +38,17 @@ class Language {
       const pText = $paragraph.text();
       $element.removeClass('langs_active');
       e.stopPropagation();
-      $target.trigger('change-language', [pText]);
+      $target.trigger('change-language', [data, pText]);
 
+    })
+  }
+
+  changeLang = (e, data, pText) => {
+    const {texts} = data.menu;
+    console.log(pText);
+    texts.forEach((e, index) => {
+      const text = texts[index][pText];
+      $(`[data-trnslt=${index}]`).text(text);
     })
   }
 }
